82d73d8f8b5b2673b8adb1eff4ba1bd87a22642d
sumitskj <sumitjangirdss.1@gmail.com>
2023-06-30 01:28:09 +0530

    bug fixes

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,27 +1,56 @@
-import { Grid, Typography } from '@mui/material';
+import { AppBar, Button, Grid, Toolbar, Typography } from '@mui/material';
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom';
+import { LogoWithName } from './components/logo/Logo';
 
 const ErrorPage = () => {
   const error = useRouteError();
+  const navigate = useNavigate();
   console.error(error);
 
   return (
-    <Grid
-      container
-      sx={{ height: '100vh' }}
-      direction='row'
-      justifyContent='center'
-      alignItems='center'>
-      <Grid item xs={12} sx={{ textAlign: 'center' }}>
-        <Typography variant='h3' gutterBottom>
-          Oops!
-        </Typography>
-        <Typography variant='body1'>
-          Something unexpected happened! We are looking into that!
-        </Typography>
+    <>
+      <AppBar position='relative' sx={{ backgroundColor: '#FFECEC' }}>
+        <Toolbar disableGutters>
+          <div
+            style={{ marginLeft: '1rem', cursor: 'pointer' }}
+            onClick={() => navigate('/', { replace: 'true' })}>
+            <LogoWithName />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Grid
+        container
+        sx={{ height: '100vh' }}
+        direction='row'
+        justifyContent='center'
+        alignItems='center'>
+        <Grid item xs={12} sx={{ textAlign: 'center' }}>
+          <Typography variant='h3' gutterBottom>
+            Oops!
+          </Typography>
+          <Typography variant='body1'>
+            {error.status === 404 && '404 Page not found.'}
+            {error.status !== 404 && 'Something unexpected happened! We are looking into that!'}
+          </Typography>
+          <Button
+            sx={{
+              m: '1rem',
+              backgroundColor: 'black',
+              padding: '10px',
+              color: 'white',
+              transition: 'all 0.2s ease-in-out',
+              ':hover': {
+                transform: 'scale(1.05)',
+                backgroundColor: 'black',
+              },
+            }}
+            onClick={() => navigate('/', { replace: 'true' })}>
+            Go Home
+          </Button>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
